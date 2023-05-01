587ce7ec6a130fdfdd912f03a067296a67c2c06d
Viktor Laishev <130911518+ViktorLaishev@users.noreply.github.com>
2023-05-01 12:13:17 +0200

    Update BruteCollinearPoints.java

--- BruteCollinearPoints.java
+++ BruteCollinearPoints.java
@@ -2,7 +2,7 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdDraw; 
 import edu.princeton.cs.algs4.StdOut; 
 
-import java.util.Arrays; 
+import java.util.Arrays;  
  
 public final class BruteCollinearPoints {  
     
